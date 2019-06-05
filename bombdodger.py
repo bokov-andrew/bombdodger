1e44633599d5ebb45cdd3a89d65a8805f4969cb6
A Bokov <lively.bokov@gmail.com>
2019-06-05 18:36:03 -0500

    resolved error #1

--- bombdodger.py
+++ bombdodger.py
@@ -34,12 +34,52 @@ def layout_window(window):
     for rowNumber, rowList in enumerate(bombfield):
         for columnNumber, columnEntry in enumerate(rowList):
             if random.randint(1,100) < 25:
-                square = tkinter.Label(window, text = "    ", bg = "red")
+                square = tkinter.Label(window, text = "    ", bg = 'darkgreen')
             elif random.randint(1,100) > 75:
                 square = tkinter.Label(window, text = '    ', bg = 'seagreen')
             else:
                 square = tkinter.Label(window, text = '    ', bg = 'green')
 
-                square.grid(row = rowNumber, column = columnNumber)
+            square.grid(row = rowNumber, column = columnNumber)
+            square.bind('<Button-1>', on_click)
 
+def on_click(event):
+    global score
+    global gameOver
+    global squaresToClear
+    square = event.widget
+    row = int(square.grid_info()['row'])
+    column = int(square.grid_info()['column'])
+    currentText = square.cget('text')
+    if gameOver == False:
+        if bombfield[row][column] == 1:
+            gameOver = True
+            square.config(bg = 'red')
+            print('wasted!')
+            print('Your score was... :', score)
+        elif currentText == '    ':
+            square.config(bg = 'brown')
+            totalBombs = 0
+            if row < 9:
+                if bombfield[row+1][column] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0:
+                if bombfield[row-1][column] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0:
+                if bombfield[row][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            if row < 9:
+                if bombfield[row][column+1] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0 and column > 0:
+                if bombfield[row-1][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            if row < 9 and column > 0:
+                if bombfield[row+1][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            if row > 0 and column < 9:
+                if bombfield[row+1][column-1] == 1:
+                    totalBombs = totalBombs + 1
+            
 play_bombdodger()
